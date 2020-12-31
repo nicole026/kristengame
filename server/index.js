ed90d972f9342b79ad4718df56d6a78e1aeb889a
Nicole Mastro <69874635+nicole026@users.noreply.github.com>
2020-12-31 14:28:41 -0500

    commit

--- server/index.js
+++ server/index.js
@@ -95,16 +95,16 @@ const createApp = () => {
   })
 }
 
-// const startListening = () => {
-//   // start listening (and create a 'server' object representing our server)
-//   const server = app.listen(PORT, () =>
-//     console.log(`Mixing it up on port ${PORT}`)
-//   )
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () =>
+    console.log(`Mixing it up on port ${PORT}`)
+  )
 
-//   // set up our socket control center
-//   // const io = socketio(server)
-//   // require('./socket')(io)
-// }
+  //   // set up our socket control center
+  //   // const io = socketio(server)
+  //   // require('./socket')(io)
+}
 
 // const syncDb = () => db.sync()
 
@@ -112,7 +112,7 @@ async function bootApp() {
   // await sessionStore.sync()
   // await syncDb()
   await createApp()
-  // await startListening()
+  await startListening()
 }
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
